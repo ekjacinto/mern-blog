29fd49e9ba54332622f042de9694667ea198ba76
Ethan Jacinto <ethankylejacinto@gmail.com>
2023-12-18 15:53:13 -0800

    feat: added routing with react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,19 @@
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout.js";
+import HomePage from "./pages/HomePage.js";
+import LoginPage from "./pages/LoginPage.js";
+import RegisterPage from "./pages/RegisterPage.js";
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">MyBlog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=990&crop=1" alt="" />
-        </div>
-        <div className="text">
-          <h2>Full-house battery backup coming later this year</h2>
-          <p className="info">
-            <a className="author">Author</a>
-            <time>12/18/2023 5:30</time>
-          </p>
-          <p className="summary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=990&crop=1" alt="" />
-        </div>
-        <div className="text">
-          <h2>Full-house battery backup coming later this year</h2>
-          <p className="info">
-            <a className="author">Author</a>
-            <time>12/18/2023 5:30</time>
-          </p>
-          <p className="summary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=990&crop=1" alt="" />
-        </div>
-        <div className="text">
-          <h2>Full-house battery backup coming later this year</h2>
-          <p className="info">
-            <a className="author">Author</a>
-            <time>12/18/2023 5:30</time>
-          </p>
-          <p className="summary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Post from "../Post.js";
+
+export default function HomePage() {
+  return (
+    <>
+      <Post />
+      <Post />
+      <Post />
+    </>
+  )  
+}
