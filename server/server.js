550d18511f6a036f7a5e25d6c318bdf3d4000e38
Ethan Jacinto <ethankylejacinto@gmail.com>
2023-12-19 12:17:03 -0800

    feat: setup login using express && mongo.db

--- server/server.js
+++ server/server.js
@@ -3,13 +3,15 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const app = express();
 
 const salt = bcrypt.genSaltSync(10);
+const secret = bcrypt.genSaltSync(10);
 
 require("dotenv").config();
 
-app.use(cors());
+app.use(cors({ credentials:true, origin:"http://localhost:3000"}));
 app.use(express.json());
 
 mongoose.connect(process.env.DB_CONNECTION_STRING);
@@ -24,6 +26,21 @@ app.post("/register", async (req, res) => {
     res.json(userDoc);
   } catch(e) {
     res.status(400).json(e)
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username })
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if(passOk) {
+    // logged in
+    jwt.sign({ username, id:userDoc._id }, secret, {}, (err, token) => {
+      if(err) throw err;
+      res.cookie("token", token).json("ok");
+    });
+  } else {
+    res.status(400).json("Wrong credentials")
   }
 });
 
