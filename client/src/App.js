40f17d3f3aebf24169bbbc0b81e774ac0385d413
Ethan Jacinto <ethankylejacinto@gmail.com>
2024-01-09 23:19:59 -0800

    feat: CreatePost component (wip) + use react-quill

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import HomePage from "./pages/HomePage.js";
 import LoginPage from "./pages/LoginPage.js";
 import RegisterPage from "./pages/RegisterPage.js";
 import { UserContextProvider } from "./UserContext.js";
+import CreatePost from "./pages/CreatePost.js";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route index element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/create" element={<CreatePost />} />
         </Route>
       </Routes>
     </UserContextProvider>
