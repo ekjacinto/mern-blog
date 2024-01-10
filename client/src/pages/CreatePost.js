afc59b468be1cbd168c3ddf4934deef7312abc30
Ethan Jacinto <ethankylejacinto@gmail.com>
2024-01-10 00:53:53 -0800

    fix: commit not including CreatePost.js changes

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { Navigate } from "react-router-dom";
 
 const modules = {
   toolbar: [
@@ -35,31 +36,55 @@ export default function CreatePost() {
   const [title, setTitle] = useState("");
   const [summary, setSummary] = useState("");
   const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
+  const [redirect, setRedirect] = useState(false);
 
+  async function createNewPost(e) {
+    const data = new FormData();
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    data.set("file", files[0])
+    e.preventDefault();
+    const response = await fetch("http://localhost:4000/post", {
+      method: "POST",
+      body: data,
+    });
+    if(response.ok) {
+      setRedirect(true);
+    }
+  }
+  
+  if(redirect) {
+    return <Navigate to={"/"} />
+  }
+  
   return (
-    <form>
-      <input 
-        type="title" 
-        placeholder={"Title"} 
-        value={title} 
+    <form onSubmit={createNewPost}>
+      <input
+        type="title"
+        placeholder={"Title"}
+        value={title}
         onChange={(e) => setTitle(e.target.value)}
       />
-      
-      <input 
-        type="summary" 
+
+      <input
+        type="summary"
         placeholder={"Summary"}
         value={summary}
         onChange={(e) => setSummary(e.target.value)}
       />
 
-      <input type="file" />
-      
-      <ReactQuill 
-        value={content}
-        onChange = {(newValue) => setContent(newValue)}
-        modules={modules} 
-        formats={formats} 
+      <input 
+        type="file" 
+        onChange={(e) => {setFiles(e.target.files)}}
+      />
 
+      <ReactQuill
+        value={content}
+        onChange={(newValue) => setContent(newValue)}
+        modules={modules}
+        formats={formats}
       />
 
       <button style={{ marginTop: "5px" }}>Create Post</button>
