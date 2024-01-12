2d2082b773613b2bbe6657b3a8bfdd8e60618a7c
Ethan Jacinto <ethankylejacinto@gmail.com>
2024-01-12 00:00:24 -0800

    feat: display image posts from db (finished)

--- client/src/Post.js
+++ client/src/Post.js
@@ -2,19 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { formatISO9075 } from "date-fns";
 
-export default function Post({ title, summary, cover, content, createdAt }) {
+export default function Post({ title, summary, cover, content, createdAt, author }) {
   return (
     <div className="post">
       <div className="image">
         <img
-          src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=990&crop=1"
+          src={`http://localhost:4000/${cover}`}
           alt=""
         />
       </div>
       <div className="text">
         <h2>{title}</h2>
         <p className="info">
-          <Link className="author">Author</Link>
+          <Link className="author">{author.username}</Link>
           <time>{formatISO9075(new Date(createdAt))}</time>
         </p>
         <p className="summary">{summary}</p>
